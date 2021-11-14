1992282a0d48fa9000c8602b8639f49982754afc
Maksim Partsiankou <mobooss@gmail.com>
2021-11-14 03:19:40 +0300

    refactor

--- Program.cs
+++ Program.cs
@@ -1,96 +1,81 @@
-﻿using System;
+using System;
 
-namespace fith
+class Table
 {
-    class Program
+    int[,] map = new int[3, 3];
+
+    public Table()
     {
-        class Table
+        int n = 0;
+
+        while (n < 9)
         {
-            int[,] map = new int[3, 3];
-            string answer = "";
+            int x = new Random().Next(map.Length / 3);
+            int y = new Random().Next(map.Length / 3);
 
-            public Table()
-            {
-                Random rand = new Random();
-                int n = 0;
-
-                while (n < 9)
-                {
-                    int x = rand.Next(map.Length / 3);
-                    int y = rand.Next(map.Length / 3);
-
-                    if (map[x, y] == 0) map[x, y] = n++;
-                }
-
-                for (int i = 1; i < map.Length / 2 + 1; i++)
-                    answer += i.ToString();
-
-                answer += '0';
-            }
-
-            public void Show()
-            {
-                for (int i = 0; i < map.Length / 3; i++)
-                {
-                    for (int j = 0; j < map.Length / 3; j++)
-                        if (map[i, j] != 0) Console.Write(map[i, j] + " ");
-                        else Console.Write("  ");
-
-                    Console.WriteLine();
-                }
-            }
-            public bool Move(ConsoleKeyInfo key) 
-            {
-                for (int i = 0; i < map.Length / 3; i++)
-                    for (int j = 0; j < map.Length / 3; j++)
-                        if (map[i, j] == 0)
-                            switch (key.Key.ToString())
-                            {
-                                case "LeftArrow": TryMove(i, j + 1, i, j); return Check();
-                                case "UpArrow": TryMove(i + 1, j, i, j); ; return Check();
-                                case "RightArrow": TryMove(i, j - 1, i, j); ; return Check();
-                                case "DownArrow": TryMove(i - 1, j, i, j); ; return Check();
-                            }
-
-                return Check();
-            }
-
-            private void TryMove(int x1, int y1, int x2, int y2)
-            {
-                try
-                {
-                    map[x2, y2] = map[x1, y1];
-                    map[x1, y1] = 0;
-                }
-                catch { }
-            }
-
-            private bool Check()
-            {
-                string str = "";
-
-                foreach (int a in map) str += a.ToString();
-
-                return str == "123456780";
-            }
+            if (map[x, y] == 0) map[x, y] = n++;
         }
 
-        static void Main(string[] args)
+        do
         {
-            Table table = new Table();
+            ShowInConsole();
 
-            while (true)
-            {
-                table.Show();
+        } while (!Move(Console.ReadKey(true)));
+    }
 
-                if (table.Move(Console.ReadKey(true)))
-                {
-                    Console.WriteLine("Over");
-                    break;
-                }
+    public void ShowInConsole()
+    {
+        Console.Clear();
 
-                Console.Clear();
-            }
+        for (int i = 0; i < map.Length / 3; i++)
+        {
+            for (int j = 0; j < map.Length / 3; j++)
+                if (map[i, j] != 0) Console.Write(map[i, j] + " ");
+                else Console.Write("  ");
+
+            Console.WriteLine();
         }
+    }
+    public bool Move(ConsoleKeyInfo key)
+    {
+        for (int i = 0; i < map.Length / 3; i++)
+            for (int j = 0; j < map.Length / 3; j++)
+                if (map[i, j] == 0)
+                    switch (key.Key.ToString())
+                    {
+                        case "LeftArrow": TryMove(i, j + 1, i, j); return Check();
+                        case "UpArrow": TryMove(i + 1, j, i, j); return Check();
+                        case "RightArrow": TryMove(i, j - 1, i, j); return Check();
+                        case "DownArrow": TryMove(i - 1, j, i, j); return Check();
+                    }
+
+        return Check();
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        try
+        {
+            map[x2, y2] = map[x1, y1];
+            map[x1, y1] = 0;
+        }
+        catch { }
+    }
+
+    private bool Check()
+    {
+        string str = "";
+
+        foreach (int a in map) str += a.ToString();
+
+        return str == "123456780";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Table table = new Table();
     }
 }
